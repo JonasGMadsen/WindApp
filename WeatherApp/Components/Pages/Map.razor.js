4b91814abe809a6e5165c7d5d5150012416b6c83
JonasGMadsen <Jonas6070@hotmail.com>
2024-12-12 21:16:05 +0100

    Add project files.

---
+++ WeatherApp/Components/Pages/Map.razor.js
@@ -0,0 +1,20 @@
+﻿export function map_init(lat = 55.6761, lon = 12.5683, zoom = 10) {
+    var map = L.map('map').
+        setView([lat, lon], zoom);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    var marker; 
+
+    map.on('click', function (e) {
+        if (marker) {
+            map.removeLayer(marker);
+        }
+
+        marker = L.marker(e.latlng).addTo(map);
+    });
+}
+
---
+++ WeatherApp/Contollers/WindDateConteoller.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherApp.Contollers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WindDateConteoller : ControllerBase
+    {
+    }
+}
